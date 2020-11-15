f14b8973c42a1994a5e3a346a9cd0c41a3cd34ab
Stanko Dujaković <dujakovi.stanko00@gmail.com>
2020-11-15 17:10:49 +0100

    Implementacija provjere dostupnosti korisničkog imena i email-a
    Implementirane su metode za slanje upita o dostupnosti korisničkog imena i emaila. U fragmentu RegisterFragment se unutar observera provjerava sadržaj upita, u slučaju da je sadržaj različit od unesenog, izvšava se registracija. U suprotnome, korisniku se ispisuje greška.

--- app/src/main/java/com/example/smartwaiter/ui/auth/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/smartwaiter/ui/auth/register/RegisterViewModel.kt
@@ -1,13 +1,21 @@
 package com.example.smartwaiter.ui.auth.register
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.smartwaiter.repository.RegisterRepository
+import hr.foi.air.webservice.model.Korisnik
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class RegisterViewModel(
     private val repository: RegisterRepository
 ) : ViewModel(){
+
+    private val _myResponseUsername: MutableLiveData<Response<List<Korisnik>>> = MutableLiveData()
+    val myResponseUsername: LiveData<Response<List<Korisnik>>>
+        get() =  _myResponseUsername
 
     fun RegisterKorisnik(
         table: String,
@@ -23,4 +31,18 @@ class RegisterViewModel(
             repository.RegisterKorisnik(table,method,username, firstName, lastName, email, userType, password)
         }
     }
+
+    fun getUsername(
+        table: String,
+        method: String,
+        username: String,
+        operator: String,
+        email: String,
+    ){
+        viewModelScope.launch {
+            val response = repository.getUsername(table, method, username, operator, email)
+            _myResponseUsername.value = response
+        }
+    }
+
 }
