30b284b7cc3e8cabb8f7a7411eaa9eb3c32c8d7e
Tomislav Godek <61702694+tgodek@users.noreply.github.com>
2020-11-18 17:16:25 +0100

    Update HomeViewModel klase
    Izmijenjen LiveData tip iz Response u Resource te dodan Resource.Loading objekt nad myResponse varijablom za prikaz progresbar-a tijekom poziva API-a

--- app/src/main/java/com/example/smartwaiter/ui/auth/login/HomeViewModel.kt
+++ app/src/main/java/com/example/smartwaiter/ui/auth/login/HomeViewModel.kt
@@ -6,37 +6,35 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.smartwaiter.repository.AuthRepository
 import hr.foi.air.webservice.model.Korisnik
+import hr.foi.air.webservice.util.Resource
 import kotlinx.coroutines.launch
-import retrofit2.Response
 
 class HomeViewModel(
     private val authRepository: AuthRepository
 ) : ViewModel() {
 
-
-    private val _myResponse: MutableLiveData<Response<List<Korisnik>>> = MutableLiveData()
-    val myResponse: LiveData<Response<List<Korisnik>>>
-        get() =  _myResponse
-
+    private val _myResponse: MutableLiveData<Resource<List<Korisnik>>> = MutableLiveData()
+    val myResponse: LiveData<Resource<List<Korisnik>>>
+        get() = _myResponse
 
     fun getKorisnik(
         table: String,
         method: String,
         username: String,
         password: String
-    ){
+    ) {
         viewModelScope.launch {
-            val response = authRepository.getKorisnik(table,method,username, password)
+            _myResponse.value = Resource.Loading
+            val response = authRepository.getKorisnik(table, method, username, password)
             _myResponse.value = response
         }
     }
 
-    suspend fun saveAuthToken(token: String)  {
+    suspend fun saveAuthToken(token: String) {
         authRepository.saveAuthToken(token)
     }
 
-    suspend fun saveUserType(userType: String){
+    suspend fun saveUserType(userType: String) {
         authRepository.saveUserType(userType)
     }
-
 }
