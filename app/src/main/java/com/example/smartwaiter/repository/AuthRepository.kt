e090939120f586b1804c8c35b2d36d27341cded4
Tomislav Godek <61702694+tgodek@users.noreply.github.com>
2020-11-18 17:12:01 +0100

    Update klase Authrepository
    Klasa sada nasljeđuje BaseRepository klasu. U metodi getKorisnik dodan je safeApiCall za sigurno pozivanje prema API-u

--- app/src/main/java/com/example/smartwaiter/repository/AuthRepository.kt
+++ app/src/main/java/com/example/smartwaiter/repository/AuthRepository.kt
@@ -1,34 +1,25 @@
 package com.example.smartwaiter.repository
 
 import com.example.database.UserPreferences
+import hr.foi.air.webservice.AuthApi
 import hr.foi.air.webservice.RetrofitInstance
-import hr.foi.air.webservice.model.Korisnik
-import retrofit2.Response
 
-class AuthRepository(
-    private val preferences: UserPreferences
-) {
+
+class AuthRepository(private val preferences: UserPreferences) : BaseRepository() {
 
     suspend fun getKorisnik(
         table: String,
         method: String,
         username: String,
         password: String
-    ): Response<List<Korisnik>>{
-        return RetrofitInstance.api.getKorisnik(table, method, username, password)
-    }
+    ) = safeApiCall { RetrofitInstance.api.getKorisnik(table, method, username, password) }
 
-    suspend fun saveAuthToken(token: String){
+    suspend fun saveAuthToken(token: String) {
         preferences.saveAuthToken(token)
     }
 
-    suspend fun saveUserType(userType: String){
+    suspend fun saveUserType(userType: String) {
         preferences.saveUserType(userType)
     }
 
 }
-
-
-/*
-
-*/
