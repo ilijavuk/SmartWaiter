b914aa184f087befdc2e854900aac44f4a869ea5
Tomislav Godek <61702694+tgodek@users.noreply.github.com>
2020-11-18 17:08:43 +0100

    Update funkcije getkorisnik
    Promjenjen povratni tip koji vraća funkcija sa Response<List<Korisnik>>> u List<Korisnik>

--- webservice/src/main/java/hr/foi/air/webservice/AuthApi.kt
+++ webservice/src/main/java/hr/foi/air/webservice/AuthApi.kt
@@ -4,29 +4,28 @@ import hr.foi.air.webservice.model.Korisnik
 import hr.foi.air.webservice.model.Restoran
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface AuthApi {
 
     @GET("sw-api/api.php")
     suspend fun getKorisnik(
-        @Query("tablica") table : String,
-        @Query("metoda") method : String,
-        @Query("korisnicko_ime") username : String,
-        @Query("lozinka_sha256") password : String
-    ): Response<List<Korisnik>>
+        @Query("tablica") table: String,
+        @Query("metoda") method: String,
+        @Query("korisnicko_ime") username: String,
+        @Query("lozinka_sha256") password: String
+    ): List<Korisnik>
 
-   @GET("sw-api/api.php")
+    @GET("sw-api/api.php")
     suspend fun getRestorani(
-        @Query("tablica") table : String,
-        @Query("metoda") method : String,
+        @Query("tablica") table: String,
+        @Query("metoda") method: String,
     ): Response<List<Restoran>>
 
     @GET("sw-api/api.php")
     suspend fun insertRestoran(
-        @Query("tablica") table : String,
-        @Query("metoda") method : String,
+        @Query("tablica") table: String,
+        @Query("metoda") method: String,
         @Query("naziv") restaurantName: String,
         @Query("adresa") restaurantAddress: String,
         @Query("GPS_Longitude") GPS_Longitude: Double,
@@ -35,20 +34,20 @@ interface AuthApi {
 
     @GET("sw-api/api.php")
     suspend fun RegisterKorisnik(
-        @Query("tablica") table : String,
-        @Query("metoda") method : String,
-        @Query("korisnicko_ime") username : String,
-        @Query("ime") firstName : String,
-        @Query("prezime") lastName : String,
-        @Query("email") email : String,
-        @Query("tip_korisnika_id") userType : String,
-        @Query("lozinka_sha256") password : String
+        @Query("tablica") table: String,
+        @Query("metoda") method: String,
+        @Query("korisnicko_ime") username: String,
+        @Query("ime") firstName: String,
+        @Query("prezime") lastName: String,
+        @Query("email") email: String,
+        @Query("tip_korisnika_id") userType: String,
+        @Query("lozinka_sha256") password: String
     )
 
     @GET("sw-api/api.php")
     suspend fun getUsername(
-        @Query("tablica") table : String,
-        @Query("metoda") method : String,
-        @Query("korisnicko_ime") username : String,
+        @Query("tablica") table: String,
+        @Query("metoda") method: String,
+        @Query("korisnicko_ime") username: String,
     ): Response<List<Korisnik>>
 }
