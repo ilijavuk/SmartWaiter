29c703a169d20550ae00f8b79a4cab064af6b608
Mislav Vučko <mislav.vucko@gmail.com>
2020-11-23 13:48:44 +0100

    Registracija stranice na nfc
    Čitanje nfc taga koji ima URI zapis swebstranicom otvara smartwaiter

---
+++ app/src/main/java/com/example/smartwaiter/ui/restaurant/nfc/NfcFragment.kt
@@ -0,0 +1,8 @@
+package com.example.smartwaiter.ui.restaurant.nfc
+
+import androidx.fragment.app.Fragment
+import com.example.smartwaiter.R
+
+class NfcFragment : Fragment(R.layout.fragment_restaurant_home) {
+
+}
