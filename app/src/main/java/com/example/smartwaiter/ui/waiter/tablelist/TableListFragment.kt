9c5c834b7463bb2d5c749b84beaa4483523f48fc
Tomislav Godek <61702694+tgodek@users.noreply.github.com>
2020-12-15 21:18:17 +0100

    Dodana klasa TableListFragment za prikaz početnog ekrana konobaru

---
+++ app/src/main/java/com/example/smartwaiter/ui/waiter/tablelist/TableListFragment.kt
@@ -0,0 +1,14 @@
+package com.example.smartwaiter.ui.waiter.tablelist
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.smartwaiter.R
+
+class TableListFragment : Fragment(R.layout.fragment_table_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
