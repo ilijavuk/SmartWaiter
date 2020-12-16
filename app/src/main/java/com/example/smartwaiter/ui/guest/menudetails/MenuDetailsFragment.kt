cac56ca14518db1dacb852828183ba07ffc3dddc
Tomislav Godek <61702694+tgodek@users.noreply.github.com>
2020-12-16 17:40:10 +0100

    Kreiraj MenuDetailsFragment klasa koja će sadržavati logiku vezanu za detaljnog prikaza odabranog menia i dodavanja meniu u košaricu za narudžbe

---
+++ app/src/main/java/com/example/smartwaiter/ui/guest/menudetails/MenuDetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.smartwaiter.ui.guest.menudetails
+
+import androidx.fragment.app.Fragment
+import com.example.smartwaiter.R
+
+class MenuDetailsFragment : Fragment(R.layout.fragment_menu_details){
+}
