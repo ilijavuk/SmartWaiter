2dbb8ab580bc03bcf70c159d3560d3842bd9d940
Tomislav Godek <61702694+tgodek@users.noreply.github.com>
2020-12-18 20:04:07 +0100

    Uspostavljen toolbar unutar aktivnosti GuestActivity

--- app/src/main/java/com/example/smartwaiter/ui/guest/GuestActivity.kt
+++ app/src/main/java/com/example/smartwaiter/ui/guest/GuestActivity.kt
@@ -2,10 +2,13 @@ package com.example.smartwaiter.ui.guest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.smartwaiter.R
+import kotlinx.android.synthetic.main.activity_guest.*
 
 class GuestActivity : AppCompatActivity() {
 
@@ -22,6 +25,14 @@ class GuestActivity : AppCompatActivity() {
     private fun setUpNavigation() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_guest) as NavHostFragment
         navController = navHostFragment.findNavController()
+
+        setSupportActionBar(toolbarGuest)
+        toolbarGuest.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 
