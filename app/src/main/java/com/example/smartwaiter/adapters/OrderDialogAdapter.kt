207e7fb7570792ce26fd2a611efa7a0fab64efd0
Tomislav Godek <61702694+tgodek@users.noreply.github.com>
2020-12-26 14:23:47 +0100

    Kreiran list adapter za prikaz naružbe menia u Alert dialogu
    Funkcija bind služi za vezanje podatkovne klase Meal sa određenim xml elementima. Ugnježđena klasa DiffCallback služi za određivanje promjena u listi i zamjenjuje nove elemente liste sa starom kada se dogodila neka nova promjena ili se izmijenio neki atribut pojednim elementa u listi

---
+++ app/src/main/java/com/example/smartwaiter/adapters/OrderDialogAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.smartwaiter.adapters
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.smartwaiter.databinding.OrderListItemBinding
+import hr.foi.air.webservice.model.Meal
+
+class OrderDialogAdapter : ListAdapter<Meal, OrderDialogAdapter.OrderViewHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderViewHolder {
+        val binding = OrderListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return OrderViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: OrderViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    class OrderViewHolder(private val binding: OrderListItemBinding): RecyclerView.ViewHolder(binding.root){
+
+        fun bind(meal: Meal){
+            binding.apply {
+                textViewOrderMealTitle.text = meal.naziv
+                textViewOrderMealPrice.text = meal.cijena
+                Glide
+                    .with(itemView)
+                    .load(meal.slika_path)
+                    .into(imageViewOrderMeal)
+            }
+        }
+    }
+
+    class DiffCallback:DiffUtil.ItemCallback<Meal>(){
+        override fun areItemsTheSame(oldItem: Meal, newItem: Meal): Boolean =
+            oldItem.id_stavka == newItem.id_stavka
+
+
+        override fun areContentsTheSame(oldItem: Meal, newItem: Meal): Boolean =
+            oldItem == newItem
+
+    }
+}
