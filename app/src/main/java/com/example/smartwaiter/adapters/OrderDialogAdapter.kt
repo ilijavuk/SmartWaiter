e81072470fe69a81fdebd0083ad0572007361917
Tomislav Godek <61702694+tgodek@users.noreply.github.com>
2020-12-30 15:35:35 +0100

    Dodan slušač nad pojedinim elementom unutar recycler view-a koji se okida nakon pritiska na buttonRemoveMeal tipku

--- app/src/main/java/com/example/smartwaiter/adapters/OrderDialogAdapter.kt
+++ app/src/main/java/com/example/smartwaiter/adapters/OrderDialogAdapter.kt
@@ -10,7 +10,7 @@ import com.bumptech.glide.Glide
 import com.example.smartwaiter.databinding.OrderListItemBinding
 import hr.foi.air.webservice.model.Meal
 
-class OrderDialogAdapter : ListAdapter<Meal, OrderDialogAdapter.OrderViewHolder>(DiffCallback()) {
+class OrderDialogAdapter(val clickListener: (Meal) -> Unit) : ListAdapter<Meal, OrderDialogAdapter.OrderViewHolder>(DiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderViewHolder {
         val binding = OrderListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -19,12 +19,13 @@ class OrderDialogAdapter : ListAdapter<Meal, OrderDialogAdapter.OrderViewHolder>
 
     override fun onBindViewHolder(holder: OrderViewHolder, position: Int) {
         val currentItem = getItem(position)
-        holder.bind(currentItem)
+        holder.bind(currentItem, clickListener)
+
     }
 
     class OrderViewHolder(private val binding: OrderListItemBinding): RecyclerView.ViewHolder(binding.root){
 
-        fun bind(meal: Meal){
+        fun bind(meal: Meal, clickListener: (Meal) -> Unit){
             binding.apply {
                 textViewOrderMealTitle.text = meal.naziv
                 textViewOrderMealPrice.text = meal.cijena
@@ -32,6 +33,8 @@ class OrderDialogAdapter : ListAdapter<Meal, OrderDialogAdapter.OrderViewHolder>
                     .with(itemView)
                     .load(meal.slika_path)
                     .into(imageViewOrderMeal)
+
+                buttonRemoveMeal.setOnClickListener{clickListener(meal)}
             }
         }
     }
@@ -40,9 +43,7 @@ class OrderDialogAdapter : ListAdapter<Meal, OrderDialogAdapter.OrderViewHolder>
         override fun areItemsTheSame(oldItem: Meal, newItem: Meal): Boolean =
             oldItem.id_stavka == newItem.id_stavka
 
-
         override fun areContentsTheSame(oldItem: Meal, newItem: Meal): Boolean =
             oldItem == newItem
-
     }
 }
