880c90b63eabb4f818c14de6af2f6010c5293a23
Tomislav Godek <61702694+tgodek@users.noreply.github.com>
2020-12-30 15:37:11 +0100

    Dodane metode za pristup pojedinim upitima u DAO klasi

--- app/src/main/java/com/example/smartwaiter/repository/OrderRepository.kt
+++ app/src/main/java/com/example/smartwaiter/repository/OrderRepository.kt
@@ -1,8 +1,10 @@
 package com.example.smartwaiter.repository
 
+import com.example.database.db.SMDatabase
 import hr.foi.air.webservice.RetrofitInstance
+import hr.foi.air.webservice.model.Meal
 
-class OrderRepository : BaseRepository() {
+class OrderRepository(val db: SMDatabase) : BaseRepository() {
 
     suspend fun makeOrder(
         table: String,
@@ -13,5 +15,11 @@ class OrderRepository : BaseRepository() {
         amount: String,
         time: String,
     ) = safeApiCall { RetrofitInstance.api.makeOrder(table, method, user_id, restaurantTable_id, meal_id ,amount, time) }
+
+    suspend fun upsert(meal: Meal) = db.getDAO().upsert(meal)
+
+    fun getMeals() = db.getDAO().getMeals()
+
+    suspend fun deleteMeal(meal: Meal) = db.getDAO().deleteMeal(meal)
 
 }
