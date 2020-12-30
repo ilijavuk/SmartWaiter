d10e052e8ed6174979c3ff928a207850cb569756
Tomislav Godek <61702694+tgodek@users.noreply.github.com>
2020-12-30 15:30:12 +0100

    Dodana anotacija Entity nad tablicom Meal za mogućnost lokalne pohrane menia

--- webservice/src/main/java/hr/foi/air/webservice/model/Meal.kt
+++ webservice/src/main/java/hr/foi/air/webservice/model/Meal.kt
@@ -1,10 +1,16 @@
 package hr.foi.air.webservice.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
+@Entity(
+     tableName = "meals"
+)
 @Parcelize
- data class Meal (
+data class Meal (
+     @PrimaryKey(autoGenerate = false)
      val id_stavka: String,
      val naziv: String,
      val cijena: String,
