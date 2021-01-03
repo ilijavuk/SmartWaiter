2f4579e8528e88b53b350519f5ac1488678de73c
Stanko Dujaković <dujakovi.stanko00@gmail.com>
2021-01-03 20:22:50 +0100

    dodavanje Toolbar-a u aktivnosti konobara
    Unutar aktivnosti konobara dodan je toolbar koji sadrži naziv fragmenta i strijelicu za povratak na prethodni fragment ukoliko nije roditelj.

--- app/src/main/java/com/example/smartwaiter/ui/waiter/WaiterActivity.kt
+++ app/src/main/java/com/example/smartwaiter/ui/waiter/WaiterActivity.kt
@@ -5,13 +5,16 @@ import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.smartwaiter.R
 import kotlinx.android.synthetic.main.activity_restoran.*
 import kotlinx.android.synthetic.main.activity_waiter.*
+import kotlinx.android.synthetic.main.fragment_table_order.*
 
 class WaiterActivity : AppCompatActivity() {
 
@@ -24,15 +27,19 @@ class WaiterActivity : AppCompatActivity() {
 
         setUpNavigation()
 
-
-
         bottom_nav_waiter.setupWithNavController(navController)
         visibilityNavElements(navController)
+
     }
 
     private fun setUpNavigation() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_waiter) as NavHostFragment
         navController = navHostFragment.findNavController()
+
+        setSupportActionBar(toolbarWaiter)
+        setupActionBarWithNavController(navController)
+
+        toolbarWaiter.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
     }
 
     private fun visibilityNavElements(navController: NavController) {
@@ -42,6 +49,10 @@ class WaiterActivity : AppCompatActivity() {
                 else -> bottom_nav_waiter?.visibility = View.VISIBLE
             }
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
