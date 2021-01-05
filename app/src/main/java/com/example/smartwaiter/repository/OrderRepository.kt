3450b383eb8286fa697ca759659d366d286d975a
Tomislav Godek <61702694+tgodek@users.noreply.github.com>
2021-01-05 15:06:34 +0100

    Dodane metode za pristup metodama u DAO sučelju

--- app/src/main/java/com/example/smartwaiter/repository/OrderRepository.kt
+++ app/src/main/java/com/example/smartwaiter/repository/OrderRepository.kt
@@ -1,16 +1,31 @@
 package com.example.smartwaiter.repository
 
+import com.example.database.UserPreferences
 import com.example.database.db.SMDatabase
+import com.example.database.db.models.OrderedMeal
 import hr.foi.air.webservice.RetrofitInstance
-import hr.foi.air.webservice.model.Meal
 
-class OrderRepository(val db: SMDatabase) : BaseRepository() {
+class OrderRepository(val db: SMDatabase, private val preferences: UserPreferences) : BaseRepository() {
 
+    suspend fun upsert(orderedMeal: OrderedMeal) = db.getDAO().upsert(orderedMeal)
 
-    suspend fun upsert(meal: Meal) = db.getDAO().upsert(meal)
+    fun getMeals() = db.getDAO().getOrderedMeals()
 
-    fun getMeals() = db.getDAO().getMeals()
+    suspend fun deleteMeal(orderedMeal: OrderedMeal) = db.getDAO().deleteMeal(orderedMeal)
 
-    suspend fun deleteMeal(meal: Meal) = db.getDAO().deleteMeal(meal)
+    suspend fun makeOrder(
+        table: String,
+        method: String,
+        user_id: Int,
+        table_id: Int,
+        status: Int,
+        time: String,
+        meal_id: Int,
+        amount: Int
+    ) = safeApiCall { RetrofitInstance.api.makeOrder(table, method, user_id, table_id, status, time, meal_id, amount) }
+
+    suspend fun saveOrderBucket(show: Boolean) {
+        preferences.saveOrderBucket(show)
+    }
 
 }
