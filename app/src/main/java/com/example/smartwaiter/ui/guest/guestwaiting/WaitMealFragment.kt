eb07ca8291c8480d69e3a5d805fce6cb98545542
Tomislav Godek <61702694+tgodek@users.noreply.github.com>
2021-01-08 14:58:34 +0100

    Priprema fragmenta za izvršavanje akcije nakon narudžbe

---
+++ app/src/main/java/com/example/smartwaiter/ui/guest/guestwaiting/WaitMealFragment.kt
@@ -0,0 +1,7 @@
+package com.example.smartwaiter.ui.guest.guestwaiting
+
+import androidx.fragment.app.Fragment
+import com.example.smartwaiter.R
+
+class WaitMealFragment : Fragment(R.layout.fragment_waiting_meal) {
+}
