9e4b726582321b900763ab462ae4c2c982b4b047
tomsicdominik <59439842+tomsicdominik@users.noreply.github.com>
2021-02-03 20:29:54 +0100

    ucita maps

---
+++ app/src/main/java/com/example/smartwaiter/ui/guest/map/MapFragment.kt
@@ -0,0 +1,87 @@
+package com.example.smartwaiter.ui.guest.map
+
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.fragment.app.Fragment
+import com.example.smartwaiter.R
+import com.example.smartwaiter.databinding.ActivityGuestBinding.inflate
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.util.zip.Inflater
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    companion object {
+        var mapFragment : SupportMapFragment?=null
+        val TAG: String = MapFragment::class.java.simpleName
+        fun newInstance() = MapFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        var rootView = inflater.inflate(R.layout.fragment_map, container, false)
+
+        mapFragment = childFragmentManager?.findFragmentById(R.id.fallasMap) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+
+        return rootView
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap!!
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+}
+/*
+class MapFragment  : Fragment(R.layout.fragment_map), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+        super.onCreate(savedInstanceState)
+
+
+        val supportMapFragment =
+            childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        supportMapFragment!!.getMapAsync(this)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        if (googleMap != null) {
+            mMap = googleMap
+            Log.d("MAP","Yo")
+        }
+
+        Log.d("MAP","Yo2")
+        //Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+}
+*/
