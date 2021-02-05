f7be4e0dec392c0a453e8275569f831e2e82ff82
tomsicdominik <59439842+tomsicdominik@users.noreply.github.com>
2021-02-05 17:15:15 +0100

    Ucitava lokaciju korisnika i prikazuje lokale na karti

--- app/src/main/java/com/example/smartwaiter/ui/guest/map/MapFragment.kt
+++ app/src/main/java/com/example/smartwaiter/ui/guest/map/MapFragment.kt
@@ -1,87 +1,161 @@
 package com.example.smartwaiter.ui.guest.map
 
 
+import android.Manifest
+import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil.setContentView
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import com.example.smartwaiter.R
-import com.example.smartwaiter.databinding.ActivityGuestBinding.inflate
-import com.google.android.gms.maps.*
+import com.example.smartwaiter.repository.Add_mealRepository
+import com.example.smartwaiter.ui.restaurant.editMeal.EditMealModelFactory
+import com.example.smartwaiter.ui.restaurant.editMeal.EditMealViewModel
+import com.example.smartwaiter.util.handleApiError
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import java.util.zip.Inflater
+import hr.foi.air.webservice.model.Restoran
+import hr.foi.air.webservice.model.Tag
+import hr.foi.air.webservice.util.Resource
+
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private var locationManager : LocationManager? = null
+    private lateinit var viewModel: MapViewModel
+    var already_loaded = false
 
     companion object {
         var mapFragment : SupportMapFragment?=null
-        val TAG: String = MapFragment::class.java.simpleName
-        fun newInstance() = MapFragment()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        load()
         // Inflate the layout for this fragment
         var rootView = inflater.inflate(R.layout.fragment_map, container, false)
+        locationManager = activity?.getSystemService(LOCATION_SERVICE) as LocationManager?
+
+
 
         mapFragment = childFragmentManager?.findFragmentById(R.id.fallasMap) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
 
         return rootView
+
+    }
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            if(!already_loaded) {
+                val sydney = LatLng(location.latitude, location.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+                mMap?.moveCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        LatLng(
+                            location!!.latitude,
+                            location!!.longitude
+                        ), 15F
+                    )
+                )
+            }
+            already_loaded=true
+
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
+
+    fun load(){
+        val repository = Add_mealRepository()
+        val viewModelFactory = MapModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MapViewModel::class.java)
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
         mMap = googleMap!!
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+        if (activity?.let {
+                ActivityCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            } != PackageManager.PERMISSION_GRANTED && activity?.let {
+                ActivityCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            } != PackageManager.PERMISSION_GRANTED
+        ) {
 
-}
-/*
-class MapFragment  : Fragment(R.layout.fragment_map), OnMapReadyCallback {
-
-    private lateinit var mMap: GoogleMap
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-        super.onCreate(savedInstanceState)
-
-
-        val supportMapFragment =
-            childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        supportMapFragment!!.getMapAsync(this)
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-    }
-
-    override fun onMapReady(googleMap: GoogleMap?) {
-        if (googleMap != null) {
-            mMap = googleMap
-            Log.d("MAP","Yo")
         }
+        mMap.setMyLocationEnabled(true);
+        locationManager?.requestLocationUpdates(
+            LocationManager.NETWORK_PROVIDER,
+            0L,
+            0f,
+            locationListener
+        )
+        mMap?.uiSettings?.isMyLocationButtonEnabled = true 
 
-        Log.d("MAP","Yo2")
-        //Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        SetMarkers()
+
+    }
+
+    private fun GetRestaurants(){
+        viewModel.getRestorani("Lokal", "select")
+    }
+    private fun SetMarkers(){
+        GetRestaurants()
+        viewModel.myResponse.observe(viewLifecycleOwner, { response ->
+            when (response) {
+                is Resource.Success -> {
+                    val listRestaurants: MutableList<Restoran> = response.value as MutableList<Restoran>
+                    for(restoran in listRestaurants){
+                        val restaurant = LatLng(restoran.GPS_longitude, restoran.GPS_latitude)
+                        val height = 130
+                        val width = 130
+                        val bitmapdraw = resources.getDrawable(R.drawable.logo_map_pin) as BitmapDrawable
+                        val b = bitmapdraw.bitmap
+                        val smallMarker = Bitmap.createScaledBitmap(b, width, height, false)
+
+                        mMap.addMarker(
+                            MarkerOptions()
+                                .position(restaurant)
+                                .title(restoran.naziv)
+                                .snippet(restoran.adresa)
+                                .icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+
+                        )
+
+
+                    }
+
+                }
+                is Resource.Loading -> {
+                }
+                is Resource.Failure -> {
+                    handleApiError(response) { GetRestaurants() }
+                }
+            }
+        })
     }
 
 }
-*/
